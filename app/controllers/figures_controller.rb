fef76ad05a68100506f58e1b6e85ef5227f72cbc
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-11-20 18:45:26 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,24 @@
 class FiguresController < ApplicationController
 
-get '/new' do
-  erb :new
-end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,6 +1,3 @@
 class LandmarksController < ApplicationController
 
-get '/' do
-  erb :index
-end
 end
